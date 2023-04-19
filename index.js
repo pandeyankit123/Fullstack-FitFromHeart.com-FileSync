68a0f02ea6b2f440b1acd9623ad918a5dede0221
Ankit Pandey <anit.sanupandey@gmail.com>
2023-04-19 14:53:56 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const app = express();
+const port = 80;
+
+// EXPRESS SPECIFIC STUFF
+app.use('/static', express.static('static')) // For serving static files
+app.use(express.urlencoded())
+
+// PUG SPECIFIC STUFF
+app.set('view engine', 'pug') // Set the template engine as pug
+app.set('views', path.join(__dirname, 'views')) // Set the views directory
+
+// ENDPOINTS
+app.get('/', (req, res)=>{
+    res.status(200).render('index.pug');
+})
+app.post('/', (req, res)=>{
+    name = req.body.name
+    age = req.body.age
+    gender = req.body.gender
+    phone = req.body.phone
+
+    let outputToWrite = `The details of the client are as follow:-
+        Name : ${name}
+        Age : ${age}
+        Gender : ${gender}
+        Phone No. : ${phone}`
+    fs.writeFileSync('output.txt', outputToWrite)
+    const params = {'message': 'Your form has been submitted successfully'}
+    res.status(200).render('index.pug', params);
+
+})
+
+// START THE SERVER
+app.listen(port, ()=>{
+    console.log(`The application started successfully on port ${port}`);
+});
